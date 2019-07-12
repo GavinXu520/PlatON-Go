b4b84f072b5f572fe4456756984c2476bc888982
GavinXu520 <meiyan532484710@163.com>
2019-07-12 13:38:36 +0800

    Signed-off-by: GavinXu520 <meiyan532484710@163.com>

---
+++ life/resolver/builtins/build/CMakeFiles/builtins.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/ashriti3.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/ashriti3.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/divti3.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/divti3.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/fixdfti.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/fixdfti.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/fixsfti.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/fixsfti.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/fixtfti.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/fixtfti.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/fixunsdfti.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/fixunsdfti.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/fixunssfti.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/fixunssfti.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/fixunstfti.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/fixunstfti.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/floattidf.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/floattidf.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/floatuntidf.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/floatuntidf.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/modti3.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/modti3.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/multi3.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/multi3.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/udivti3.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/udivti3.o"
+  "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/umodti3.c" "/home/gavin/go-workspace/src/github.com/PlatONnetwork/PlatON-Go/life/resolver/builtins/build/CMakeFiles/builtins.dir/umodti3.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  "../../softfloat/source/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
