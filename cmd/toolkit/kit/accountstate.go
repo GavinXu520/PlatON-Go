0c406405afe76f5307be9a0921ef34e9c0cf68e7
GavinXu <meiyan532484710@163.com>
2020-03-29 12:54:31 +0800

    Signed-off-by: GavinXu <meiyan532484710@163.com>

--- cmd/toolkit/kit/accountstate.go
+++ cmd/toolkit/kit/accountstate.go
@@ -3,11 +3,14 @@ package kit
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/PlatONnetwork/PlatON-Go/common"
 	"github.com/PlatONnetwork/PlatON-Go/core/rawdb"
 	"github.com/PlatONnetwork/PlatON-Go/core/state"
+	"github.com/PlatONnetwork/PlatON-Go/crypto"
 	"github.com/PlatONnetwork/PlatON-Go/rlp"
 	"github.com/PlatONnetwork/PlatON-Go/trie"
 	"gopkg.in/urfave/cli.v1"
+	"time"
 )
 
 var (
@@ -28,7 +31,7 @@ func accountState(c *cli.Context) error {
 	number := c.Uint64(blockNumFlag.Name)
 
 	hash := rawdb.ReadCanonicalHash(ldb, number)
-	println("block hash:", hash.String())
+	println("accountstate block hash:", hash.String())
 	block := rawdb.ReadBlock(ldb, hash, number)
 	if block == nil {
 		return fmt.Errorf("find block num:%d is nil", number)
@@ -37,8 +40,29 @@ func accountState(c *cli.Context) error {
 	root := block.Root()
 	tr, err := trie.NewSecure(root, trie.NewDatabase(ldb), 0)
 	if err != nil {
+		fmt.Println("accountstate new trie is failed", "blockNumber", number, "blockHash", hash.String(), "root", root.Hex(), "err", err)
 		return err
 	}
+
+	// c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470
+	emptyCodeHash := crypto.Keccak256(nil)
+	fmt.Println("accountstate emptyHashStr", hex.EncodeToString(emptyCodeHash), "\naccountstate emptyHashHex", common.BytesToHash(emptyCodeHash).Hex())
+
+
+	emptyHashStr := hex.EncodeToString(emptyCodeHash)
+
+	totalAccountCount := 0
+	contractAccountCount := 0
+
+	start := time.Now()
+
+
+	stats := make(map[string]int, 0)
+
+	rootHash := make(map[string]struct{}, 0)
+	//
+	//defeRootHash := make(map[string][]string, 0)
+
 
 	iter := tr.NodeIterator(nil)
 	for iter.Next(true) {
@@ -48,10 +72,60 @@ func accountState(c *cli.Context) error {
 			if err != nil {
 				return fmt.Errorf("parse account error:%s", err.Error())
 			}
-			value := iter.LeafKey()
-			fmt.Println("account:", hex.EncodeToString(value), "nonce:", obj.Nonce, "codehash", hex.EncodeToString(obj.CodeHash))
+
+			if hex.EncodeToString(obj.CodeHash) != emptyHashStr /*&& "737e0f5e7391bac57b4213527b5a343f732279df47448657afac9980719ae28f" != hex.EncodeToString(obj.CodeHash)*/ {
+				value := iter.LeafKey()
+				fmt.Println("accountAddr Hash:",/* common.BytesToHash(value).String(),*/ hex.EncodeToString(value), "nonce:", obj.Nonce, "codehash", hex.EncodeToString(obj.CodeHash))
+				contractAccountCount++
+
+				codeHash := hex.EncodeToString(obj.CodeHash)
+				if c, ok := stats[codeHash]; ok {
+					c++
+					stats[codeHash] = c
+				} else {
+					stats[codeHash] = 1
+				}
+
+				crh := codeHash+"_" + obj.Root.Hex()
+
+				if _, ok := rootHash[crh]; !ok {
+					rootHash[crh] = struct{}{}
+				}
+
+
+				//if r, ok := rootHash[codeHash]; !ok {
+				//	rootHash[codeHash] = obj.Root.Hex()
+				//	defeRootHash[codeHash] = []string{obj.Root.Hex()}
+				//} else {
+				//	if r != obj.Root.Hex() {
+				//		arr := defeRootHash[codeHash]
+				//		arr = append(arr, obj.Root.Hex())
+				//	}
+				//}
+			}
+
+			//fmt.Println("accountAddr Hash:", hex.EncodeToString(value), "nonce:", obj.Nonce, "codehash", hex.EncodeToString(obj.CodeHash))
+
+			totalAccountCount++
 
 		}
 	}
+
+	duration := time.Since(start)
+	fmt.Println("using time duration:", duration)
+	fmt.Println("the contract account count:", contractAccountCount)
+	fmt.Println("the total account count:", totalAccountCount)
+
+	for k, v := range stats {
+		//arr := defeRootHash[k]
+		fmt.Println(k, v)
+	}
+
+	//for k, _ := range rootHash {
+	//	fmt.Println(k)
+	//}
+
+	fmt.Println("rootHash", len(rootHash))
+
 	return nil
 }
